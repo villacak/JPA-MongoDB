4522480bad59b153fec6e3524de277aca973376b
villacak <klausvillaca@gmail.com>
2019-05-06 13:12:52 +1000

    Application wworkng on aws sam local.

--- src/main/java/au/com/mongodb/main/ResourceConfigApp.java
+++ src/main/java/au/com/mongodb/main/ResourceConfigApp.java
@@ -1,25 +1,25 @@
-package au.com.mongodb.main;
-
-import org.glassfish.jersey.message.GZipEncoder;
-import org.glassfish.jersey.message.filtering.EntityFilteringFeature;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.server.ServerProperties;
-import org.glassfish.jersey.server.filter.EncodingFilter;
-
-import javax.ws.rs.ApplicationPath;
-
-public class ResourceConfigApp {
-
-    @ApplicationPath("/rest")
-    public class Application extends ResourceConfig {
-        public Application() {
-            packages("au.com.mongodb.services.v1.health", "au.com.mongodb.services.v1.crud");
-            register(EntityFilteringFeature.class);
-
-            // Some logs to see request been received
-//            register(new LoggingFilter(java.util.logging.Logger.getLogger("STANDARD_MESSAGE"), true));
-            EncodingFilter.enableFor(this, GZipEncoder.class);
-            property(ServerProperties.TRACING, "ALL");
-        }
-    }
-}
+//package au.com.mongodb.main;
+//
+//import org.glassfish.jersey.message.GZipEncoder;
+//import org.glassfish.jersey.message.filtering.EntityFilteringFeature;
+//import org.glassfish.jersey.server.ResourceConfig;
+//import org.glassfish.jersey.server.ServerProperties;
+//import org.glassfish.jersey.server.filter.EncodingFilter;
+//
+//import javax.ws.rs.ApplicationPath;
+//
+//public class ResourceConfigApp {
+//
+//    @ApplicationPath("/rest")
+//    public class Application extends ResourceConfig {
+//        public Application() {
+//            packages("au.com.mongodb.services.v1.health", "au.com.mongodb.services.v1.crud");
+//            register(EntityFilteringFeature.class);
+//
+//            // Some logs to see request been received
+////            register(new LoggingFilter(java.util.logging.Logger.getLogger("STANDARD_MESSAGE"), true));
+//            EncodingFilter.enableFor(this, GZipEncoder.class);
+//            property(ServerProperties.TRACING, "ALL");
+//        }
+//    }
+//}
--- src/main/java/au/com/mongodb/services/v1/health/HealthService.java
+++ src/main/java/au/com/mongodb/services/v1/health/HealthService.java
@@ -1,5 +1,9 @@
 package au.com.mongodb.services.v1.health;
 
+import au.com.mongodb.model.ResponseMessage;
+import jdk.net.SocketFlow;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -7,6 +11,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/v1/health")
+@Consumes(MediaType.WILDCARD)
 @Produces({MediaType.TEXT_HTML, MediaType.APPLICATION_JSON})
 public class HealthService {
 
@@ -14,8 +19,8 @@ public class HealthService {
     @GET
     @Path("check")
     public Response checkHealth() {
-        final String responseMessage = "Service working.";
-        final Response response = Response.ok().entity(responseMessage).build();
+        final ResponseMessage rs = new ResponseMessage(Response.Status.OK.getStatusCode(), "Service working.");
+        final Response response = Response.ok().entity(rs.toJSON()).build();
         return response;
     }
 
@@ -27,11 +32,11 @@ public class HealthService {
         final Response response;
         final HealthHelper helper = new HealthHelper();
         if (helper.checkifMongoIsRunning()) {
-            responseMessage = "MongoDB is Up&Running.";
-            response = Response.ok().entity(responseMessage).build();
+            final ResponseMessage rs = new ResponseMessage(Response.Status.OK.getStatusCode(), "MongoDB is Up&Running.");
+            response = Response.ok().entity(rs.toJSON()).build();
         } else {
-            responseMessage = "MongoDB is down.";
-            response = Response.status(Response.Status.SERVICE_UNAVAILABLE).entity(responseMessage).build();
+            final ResponseMessage rs = new ResponseMessage(Response.Status.OK.getStatusCode(), "MongoDB is down.");
+            response = Response.status(Response.Status.SERVICE_UNAVAILABLE).entity(rs.toJSON()).build();
         }
         return response;
     }
